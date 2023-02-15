b51b855da3802eccde9671dcf6ab0f3ad1625f70
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-02-15 16:14:22 -0300

    feat: add User model for db

---
+++ server/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('../db/conn')
+const { Schema } = mongoose
+
+const User = mongoose.model(
+  'User',
+  new Schema(
+    {
+      name: {
+        type: String,
+        required: true
+      },
+      email: {
+        type: String,
+        required: true
+      },
+      password: {
+        type: String,
+        required: true
+      },
+      phone: {
+        type: String,
+        required: true
+      },
+      block: {
+        type: String,
+        required: true
+      },
+      apartment: {
+        type: String,
+        required: true
+      },
+      image: {
+        type: String
+      }
+    },
+    { timestamps: true }
+  )
+)
+
+module.exports = User
