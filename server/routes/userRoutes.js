675b883b149e3952cd84e855b147ed519c36cdd8
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-02-23 11:29:01 -0300

    feat: add Routes (/:id to userRoutes)

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,5 +4,6 @@ const UserController = require('../controllers/UserController')
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/check', UserController.check)
+router.get('/:id', UserController.getUserById)
 
 module.exports = router
