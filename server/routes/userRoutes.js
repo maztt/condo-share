fede0f26dc7fe6f3a434dae732004c17be120ede
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-02-24 16:53:54 -0300

    feat: add Middleware to /edit/:id

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,13 @@
 const router = require('express').Router()
 const UserController = require('../controllers/UserController')
 
+// Middlewares
+const verifyToken = require('../helpers/verify-user-token')
+
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/check', UserController.check)
 router.get('/:id', UserController.getUserById)
+router.patch('/edit/:id', verifyToken, UserController.editUser)
 
 module.exports = router
