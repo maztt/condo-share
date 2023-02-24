5e90c1b7577b2da0a477dfef92cac826243dc1b1
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-02-24 17:51:43 -0300

    feat: add Helper image-upload so users can upload photos

---
+++ server/helpers/image-upload.js
@@ -0,0 +1,31 @@
+const multer = require('multer')
+const path = require('path')
+
+const imageStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    let folder = ''
+
+    if (req.baseUrl.includes('users')) {
+      folder = 'users'
+    } else if (req.baseUrl.includes('tools')) {
+      folder = 'tools'
+    }
+
+    cb(null, `public/images/${folder}`)
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname))
+  }
+})
+
+const imageUpload = multer({
+  storage: imageStorage,
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(png|jpg)$/)) {
+      return cb(new Error('Only JPEG and PNG files are allowed.'))
+    }
+    cb(undefined, true)
+  }
+})
+
+module.exports = { imageUpload }
