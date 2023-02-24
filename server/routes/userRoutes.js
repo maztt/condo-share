e1697cbd5f57402e19f88e2e1c20f5bc7f549be0
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-02-24 17:53:42 -0300

    feat: add Middleware to /edit/:id

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,11 +3,17 @@ const UserController = require('../controllers/UserController')
 
 // Middlewares
 const verifyToken = require('../helpers/verify-user-token')
+const { imageUpload } = require('../helpers/image-upload')
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/check', UserController.check)
 router.get('/:id', UserController.getUserById)
-router.patch('/edit/:id', verifyToken, UserController.editUser)
+router.patch(
+  '/edit/:id',
+  verifyToken,
+  imageUpload.single('image'),
+  UserController.editUser
+)
 
 module.exports = router
