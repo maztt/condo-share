060cca71d3d337cf0ece5b184d48d4b2049c1d64
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-02-24 17:29:18 -0300

    feat: allow users to edit their user data

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken')
 
 const createUserToken = require('../helpers/create-user-token')
 const getToken = require('../helpers/get-user-token')
+const getUserByToken = require('../helpers/get-user-by-token')
 
 module.exports = class UserController {
   static async register (req, res) {
@@ -181,7 +182,8 @@ module.exports = class UserController {
 
     const id = req.params.id
 
-    const user = await User.findById(id)
+    const token = getToken(req)
+    const user = await getUserByToken(token)
 
     const { name, email, password, confirmpassword, phone, block, apartment } =
       req.body
@@ -196,6 +198,8 @@ module.exports = class UserController {
       return
     }
 
+    user.name = name
+
     if (!email) {
       res.status(422).json({
         message: 'You must specify an email!'
@@ -203,60 +207,68 @@ module.exports = class UserController {
       return
     }
 
-    const userExists = await User.findOne({email: email})
+    const userExists = await User.findOne({ email: email })
 
     if (user.email !== email && userExists) {
       res.status(422).json({
-        message: 'User not found.'
+        message: 'Please, try other email.'
       })
       return 
     }
 
     user.email = email
 
-
-    if (!password) {
-      res.status(422).json({
-        message: 'Pick a password for your account!'
-      })
-      return
-    }
-
-    if (!confirmpassword) {
-      res.status(422).json({
-        message: 'You must confirm your password!'
-      })
-      return
-    }
-
-    if (password !== confirmpassword) {
-      res.status(422).json({
-        message: 'The passwords are not matching.'
-      })
-      return
-    }
-
+    
     if (!phone) {
       res.status(422).json({
         message: 'You must inform your phone!'
       })
       return
     }
-
+    
+    user.phone = phone
+    
     if (!block) {
       res.status(422).json({
         message: 'You must inform the block you live!'
       })
       return
     }
-
+    
+    user.block = block
+    
     if (!apartment) {
       res.status(422).json({
         message: 'You must inform the apartment you live!'
       })
       return
     }
+    
+    user.apartment = apartment
+    
+    if (password !== confirmpassword) {
+      res.status(422).json({
+        message: 'The passwords are not matching.'
+      })
+      return
+    } else if (password === confirmpassword && password != null) {
+      const salt = await bcrypt.genSalt(12)
+      const hashedPassword = await bcrypt.hashSync(password, salt)
+  
+      user.password = hashedPassword
+    }
 
+    try {
+      await User.findOneAndUpdate(
+        {_id: user._id},
+        { $set: user },
+        { new: true }
+      )
 
+      res.status(200).json({ message: 'User has been updated!'})
+    } catch (error) {
+      res.status(500).json({ message: error })
+      return
+    }
   }
 }
