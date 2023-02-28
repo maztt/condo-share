c9c685609b6c74f5b1096765a1ef03f0e49f4b36
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-02-28 11:56:59 -0300

    feat(layout/Navbar): apply style

--- web/src/components/layout/Navbar.js
+++ web/src/components/layout/Navbar.js
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom'
 
+import styles from './Navbar.module.css'
+
 import Logo from '../../assets/img/logo.png'
 
 function Navbar () {
   return (
-    <nav>
-      <div>
+    <nav className={styles.navbar}>
+      <div className={styles.navbar_logo}>
         <img src={Logo} alt="CondoShare" />
       </div>
       <ul>
