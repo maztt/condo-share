f5e94bdd9ac895e551c653d2867be5bd2735bd3d
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-02-28 13:37:45 -0300

    feat(layout/Input): apply style

--- web/src/components/form/Input.js
+++ web/src/components/form/Input.js
@@ -1,3 +1,5 @@
+import styles from './Input.module.css'
+
 function Input({
   type,
   text,
@@ -8,7 +10,7 @@ function Input({
   multiple
 }) {
   return (
-    <div>
+    <div className={styles.form_control}>
       <label htmlFor={name}>{text}:</label>
       <input
         type={type}
