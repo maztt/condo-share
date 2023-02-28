62fb56ec258b7d73166b27dd92b694979ef0383f
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-02-28 13:39:16 -0300

    feat: add Component Register

---
+++ web/src/components/pages/Auth/Register.js
@@ -0,0 +1,69 @@
+import { Link } from 'react-router-dom'
+
+function Register () {
+
+  function handleChange (e) {}
+
+  return (
+    <section>
+      <h1>Register</h1>
+      <form>
+        <Input 
+          text="Name"
+          type="text"
+          name="name"
+          placeholder="What's your name?"
+          handleOnChange={handleChange}
+        />
+        <Input 
+          text="Email"
+          type="email"
+          name="email"
+          placeholder="What's your email?"
+          handleOnChange={handleChange}
+        />
+        <Input 
+          text="Phone"
+          type="text"
+          name="phone"
+          placeholder="What's your phone number?"
+          handleOnChange={handleChange}
+        />
+        <Input 
+          text="Block"
+          type="text"
+          name="block"
+          placeholder="What's the block you live at?"
+          handleOnChange={handleChange}
+        />
+        <Input 
+          text="Apartment"
+          type="text"
+          name="apartment"
+          placeholder="What's the apartment you live in?"
+          handleOnChange={handleChange}
+        />
+        <Input 
+          text="Password"
+          type="password"
+          name="password"
+          placeholder="Pick a password"
+          handleOnChange={handleChange}
+        />
+        <Input 
+          text="Confirm your password"
+          type="password"
+          name="confirmpassword"
+          placeholder="Confirm the password you picked"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="Register" />
+      </form>
+      <p>
+        <Link to="/login">I have an account</Link>
+      </p>
+    </section>
+  )
+}
+
+export default Register
