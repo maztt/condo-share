92605b1c0c98d03277e99bb900d2ad36f0b86be4
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-02-28 11:57:15 -0300

    feat(layout/Footer): apply style

--- web/src/components/layout/Footer.js
+++ web/src/components/layout/Footer.js
@@ -1,6 +1,8 @@
+import styles from './Footer.module.css'
+
 function Footer () {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <p>
         <span className="bold">CondoShare</span> &copy; 2023
       </p>
