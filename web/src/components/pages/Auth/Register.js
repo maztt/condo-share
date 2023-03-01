062bf2d07c1d82e610f9b27273e539ea0e77e132
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-03-01 10:45:18 -0300

    feat(pages/Register): add form data gets sent to db

--- web/src/components/pages/Auth/Register.js
+++ web/src/components/pages/Auth/Register.js
@@ -1,60 +1,71 @@
 import Input from '../../form/Input.js'
 
 import { Link } from 'react-router-dom'
+import { useState, useContext } from 'react'
+import { Context } from '../../../context/UserContext.js'
 
 import styles from '../../form/Form.module.css'
 
-function Register () {
+function Register() {
+  const [user, setUser] = useState({})
+  const { register } = useContext(Context)
 
-  function handleChange (e) {}
+  function handleChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.value })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    register(user)
+  }
 
   return (
     <section className={styles.form_container}>
       <h1>Register</h1>
-      <form>
-        <Input 
+      <form onSubmit={handleSubmit}>
+        <Input
           text="Name"
           type="text"
           name="name"
           placeholder="What's your name?"
           handleOnChange={handleChange}
         />
-        <Input 
+        <Input
           text="Email"
           type="email"
           name="email"
           placeholder="What's your email?"
           handleOnChange={handleChange}
         />
-        <Input 
+        <Input
           text="Phone"
           type="text"
           name="phone"
           placeholder="What's your phone number?"
           handleOnChange={handleChange}
         />
-        <Input 
+        <Input
           text="Block"
           type="text"
           name="block"
           placeholder="What's the block you live at?"
           handleOnChange={handleChange}
         />
-        <Input 
+        <Input
           text="Apartment"
           type="text"
           name="apartment"
           placeholder="What's the apartment you live in?"
           handleOnChange={handleChange}
         />
-        <Input 
+        <Input
           text="Password"
           type="password"
           name="password"
           placeholder="Pick a password"
           handleOnChange={handleChange}
         />
-        <Input 
+        <Input
           text="Confirm your password"
           type="password"
           name="confirmpassword"
