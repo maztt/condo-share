4da7929b6f3ed6d9641afa73aa4559350f3a4fdd
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-03-01 12:10:23 -0300

    feat: add flash messages to useAuth responses

--- web/src/hooks/useAuth.js
+++ web/src/hooks/useAuth.js
@@ -1,20 +1,27 @@
 import api from '../utils/api.js'
 
-import {useState, useEffect} from 'react'
-import {useHistory} from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import useFlashMessage from './useFlashMessage.js'
 
 export default function useAuth() {
+  const { setFlashMessage } = useFlashMessage()
+
   async function register(user) {
+    let msgText = 'Account was created!'
+    let msgType = 'success'
 
     try {
-      await api.post('/users/register', user).then((response) => {
+      const data = await api.post('/users/register', user).then(response => {
         return response.data
-      })   
+      })
     } catch (error) {
-      console.error(error)
+      msgText = error.response.data.message
+      msgType = 'error'
     }
+
+    setFlashMessage(msgText, msgType)
   }
 
   return { register }
-
 }
