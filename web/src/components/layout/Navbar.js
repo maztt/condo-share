1ca4ce334f992e78099c8aa289a073b9da43d769
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-03-02 10:40:07 -0300

    feat(layout/Navbar): add front displays accordingly to auth

--- web/src/components/layout/Navbar.js
+++ web/src/components/layout/Navbar.js
@@ -1,25 +1,43 @@
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { Context } from '../../context/UserContext.js'
 
 import styles from './Navbar.module.css'
 
 import Logo from '../../assets/img/logo.png'
 
 function Navbar () {
+
+  const { authentication, logout } = useContext(Context)
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbar_logo}>
         <img src={Logo} alt="CondoShare" />
       </div>
       <ul>
-        <li>
-          <Link to='/register'>Register</Link>
-        </li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-        <li>
-          <Link to='/'>Claim</Link>
-        </li>
+        { authentication ? (
+          <>
+            <li>
+              <Link to='/'>Claim</Link>
+            </li>
+            <li>
+              <Link to="/user/profile">Profile</Link>
+            </li>
+            <li onClick={logout}>
+              Log off
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to='/register'>Register</Link>
+            </li>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   )
