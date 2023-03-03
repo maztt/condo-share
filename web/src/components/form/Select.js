e49f343731f355d993b98d3f0cffe32ea92c53e9
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-03-03 11:00:37 -0300

    feat(form/Select): apply style

--- web/src/components/form/Select.js
+++ web/src/components/form/Select.js
@@ -1,10 +1,17 @@
-function Select({text, name, options, handleOnChange, value}) {
+import styles from './Select.module.css'
+
+function Select({ text, name, options, handleOnChange, value }) {
   return (
-    <div>
+    <div className={styles.form_control}>
       <label htmlFor="{name}">{text}:</label>
-      <select name={name} id={name} onChange={handleOnChange} value={value || ''}>
+      <select
+        name={name}
+        id={name}
+        onChange={handleOnChange}
+        value={value || ''}
+      >
         <option>Select an option</option>
-        {options.map((option) => (
+        {options.map(option => (
           <option value="{option}" key={option}>
             {option}
           </option>
