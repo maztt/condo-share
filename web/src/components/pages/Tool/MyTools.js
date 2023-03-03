c1e801cd283919d5ec72e54972070a771b7b58f3
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-03-03 13:22:37 -0300

    refactor(pages/MyTools): make code more readable

--- web/src/components/pages/Tool/MyTools.js
+++ web/src/components/pages/Tool/MyTools.js
@@ -11,31 +11,36 @@ function MyTools() {
   const { setFlashMessage } = useFlashMessage()
 
   useEffect(() => {
-    api.get('tools/mytools', {
-      headers: {
-        Authorization: `Bearer ${JSON.parse(token)}`
-      }
-    }).then(response => {
-      setTools(response.data.tools)
-    })
+    api
+      .get('tools/mytools', {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`
+        }
+      })
+      .then(response => {
+        setTools(response.data.tools)
+      })
   }, [token])
 
   async function removeTool(id) {
     let msgType = 'success'
 
-    const data = await api.delete(`/tools/${id}`, {
-      headers: {
-        Authorization: `Bearer ${JSON.parse(token)}`
-      }
-    }).then((response) => {
-      const updatedTools = tools.filter((tool) => tool._id != id)
-      setTools(updatedTools)
+    const data = await api
+      .delete(`/tools/${id}`, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`
+        }
+      })
+      .then(response => {
+        const updatedTools = tools.filter(tool => tool._id != id)
+        setTools(updatedTools)
 
-      return response.data
-    }).catch((err) => {
-      msgType = 'error'
-      return err.response.data
-    })
+        return response.data
+      })
+      .catch(err => {
+        msgType = 'error'
+        return err.response.data
+      })
 
     setFlashMessage(data.message, msgType)
   }
@@ -47,31 +52,38 @@ function MyTools() {
         <Link to="/tool/add">Publish a new Tool</Link>
       </div>
       <div className={styles.petslist_container}>
-        {tools.length > 0 && tools.map((tool) => (
-          <div key={tool._id} className={styles.petlist_row}>
-            <RoundedImage 
+        {tools.length > 0 &&
+          tools.map(tool => (
+            <div key={tool._id} className={styles.petlist_row}>
+              <RoundedImage
                 src={`${process.env.REACT_APP_API}/images/tools/${tool.images[0]}`}
                 alt={tool.name}
                 width="px75"
-            />
-            <span className="bold">{tool.name}</span>
-            <div className={styles.actions}>
-              {tool.available ? (
-                <>
-                  {tool.taker && (
-                    <button className={styles.conclude_btn}>Confirm Claim</button>
-                  )}
-                  <Link to={`/tool/edit/${tool._id}`}>Edit</Link>
-                  <button onClick={() => {
-                    removeTool(tool._id)
-                  }}>Delete</button>
-                </>
-              ) : (
-                <p>This tool has already been claimed.</p>
-              )}
+              />
+              <span className="bold">{tool.name}</span>
+              <div className={styles.actions}>
+                {tool.available ? (
+                  <>
+                    {tool.taker && (
+                      <button className={styles.conclude_btn}>
+                        Confirm Claim
+                      </button>
+                    )}
+                    <Link to={`/tool/edit/${tool._id}`}>Edit</Link>
+                    <button
+                      onClick={() => {
+                        removeTool(tool._id)
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </>
+                ) : (
+                  <p>This tool has already been claimed.</p>
+                )}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
         {tools.length === 0 && <p>You have not published any tools.</p>}
       </div>
     </section>
