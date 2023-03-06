50ae3532c908a93c915e2fc57f4083bfa796baa6
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-03-06 12:26:43 -0300

    feat: add Routes to main

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,44 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from './components/layout/Navbar.js'
+import Message from "./components/layout/Message.js";
+import Footer from './components/layout/Footer.js'
+import Container from './components/layout/Container.js'
+
+import Register from './components/pages/Auth/Register.js'
+import Login from './components/pages/Auth/Login.js'
+import Home from './components/pages/Home.js'
+import Profile from './components/pages/User/Profile.js'
+import MyTools from './components/pages/Tool/MyTools.js'
+import AddTool from './components/pages/Tool/AddTool.js'
+import EditTool from './components/pages/Tool/EditTool.js'
+import MyTakings from "./components/pages/Tool/MyTakings.js";
+import ToolDetails from "./components/pages/Tool/ToolDetails.js";
+
+import { UserProvider } from './context/UserContext.js'
+
 function App() {
   return (
-    <div className="App">  
-    </div>
+    <Router>
+      <UserProvider>
+        <Navbar />
+          <Message />
+          <Container>
+            <Routes>
+              <Route path='/register' element={<Register />}/>
+              <Route path='/login' element={<Login />}/>
+              <Route path='/user/profile' element={<Profile />}/>
+              <Route path='/tool/mytools' element={<MyTools />}/>
+              <Route path='/tool/add' element={<AddTool />}/>
+              <Route path='/tool/edit/:id' element={<EditTool />}/>
+              <Route path='/tool/:id' element={<ToolDetails />}/>
+              <Route path='/tool/mytakings' element={<MyTakings />}/>
+              <Route path='/' element={<Home />}/>
+            </Routes>
+          </Container>
+        <Footer />
+      </UserProvider>
+    </Router>
   );
 }
 
