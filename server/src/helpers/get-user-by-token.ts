ee1fc457d31ddb779b6a0b9d1a69365771120c1f
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-09-12 17:14:42 -0300

    fix: getUserByToken was failing to properly return the logged in user

--- server/src/helpers/get-user-by-token.ts
+++ server/src/helpers/get-user-by-token.ts
@@ -1,13 +1,19 @@
-import jwt from 'jsonwebtoken'
+import jwt, { JwtPayload } from 'jsonwebtoken'
 import User from '../models/User'
 import { Response } from 'express'
+import mongoose from 'mongoose'
+
+const ObjectId = mongoose.Types.ObjectId
 
 const getUserByToken = async (token: string, res: Response): Promise<any> => {
   if (!token) {
     return res.status(401).json({ message: 'Access denied.' })
   }
-  const data = jwt.verify(token, 'dasecret')
-  return await User.findOne({ data })
+  const { id } = jwt.verify(token, 'dasecret') as JwtPayload
+  if (!ObjectId.isValid(id)) {
+    return false
+  }
+  return await User.findById(id)
 }
 
 export { getUserByToken }
