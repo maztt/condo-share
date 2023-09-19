e4a957ba27c221e214113613bf8c673e0c440f9e
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-09-19 13:56:00 -0300

    chore: rename all MyTakings to MyClaimings for a clearer meaning

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Navbar from './components/layout/Navbar.js'
-import Message from "./components/layout/Message.js";
+import Message from './components/layout/Message.js'
 import Footer from './components/layout/Footer.js'
 import Container from './components/layout/Container.js'
 
@@ -12,34 +12,37 @@ import Profile from './components/pages/User/Profile.js'
 import MyTools from './components/pages/Tool/MyTools.js'
 import AddTool from './components/pages/Tool/AddTool.js'
 import EditTool from './components/pages/Tool/EditTool.js'
-import MyTakings from "./components/pages/Tool/MyTakings.js";
-import ToolDetails from "./components/pages/Tool/ToolDetails.js";
+import MyClaimings from './components/pages/Tool/MyClaimings.js'
+import ToolDetails from './components/pages/Tool/ToolDetails.js'
 
 import { UserProvider } from './context/UserContext.js'
 
 function App() {
-  return (
-    <Router>
-      <UserProvider>
-        <Navbar />
-          <Message />
-          <Container>
-            <Routes>
-              <Route path='/login' element={<Login />}/>
-              <Route path='/register' element={<Register />}/>
-              <Route path='/user/profile' element={<Profile />}/>
-              <Route path='/tool/add' element={<AddTool />}/>
-              <Route path='/tool/edit/:id' element={<EditTool />}/>
-              <Route path='/tool/mytools' element={<MyTools />}/>
-              <Route path='/tool/mytakings' element={<MyTakings />}/>
-              <Route path='/tool/:id' element={<ToolDetails />}/>
-              <Route path='/' element={<Home />}/>
-            </Routes>
-          </Container>
-        <Footer />
-      </UserProvider>
-    </Router>
-  );
+    return (
+        <Router>
+            <UserProvider>
+                <Navbar />
+                <Message />
+                <Container>
+                    <Routes>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/user/profile" element={<Profile />} />
+                        <Route path="/tool/add" element={<AddTool />} />
+                        <Route path="/tool/edit/:id" element={<EditTool />} />
+                        <Route path="/tool/mytools" element={<MyTools />} />
+                        <Route
+                            path="/tool/myclaimings"
+                            element={<MyClaimings />}
+                        />
+                        <Route path="/tool/:id" element={<ToolDetails />} />
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </Container>
+                <Footer />
+            </UserProvider>
+        </Router>
+    )
 }
 
-export default App;
+export default App
